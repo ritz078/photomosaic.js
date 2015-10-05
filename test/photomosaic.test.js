c165bfa3630c77de69fadd75fa84e3ba23e4eb06
Ritesh Kumar <rkritesh078@gmail.com>
2015-10-06 01:39:09 +0530

    refactor(rgbToHex): removed the function and solved jshint issues

--- test/photomosaic.test.js
+++ test/photomosaic.test.js
@@ -10,47 +10,27 @@ describe('PhotoMosaic', function() {
 
         it('should be present in the window object', function() {
             expect(window.PhotoMosaic).to.exist;
-        })
-    })
+        });
+    });
 
     describe('Utility functions', function() {
-        describe('rgbToHex() method', function() {
-            var rgb = {
-            	r:55,
-            	g:34,
-            	b:23
-            }
-
-            var photomosaic = new PhotoMosaic({
-                'image': image
-            });
-
-            it('should return a string', function() {
-                expect(photomosaic.rgbToHex(rgb)).to.be.a('string')
-            });
-
-            it('should return the correct result', function() {
-                expect(photomosaic.rgbToHex(rgb)).to.equal('372217')
-            });
-        })
-
         describe('extend() method', function () {
             var a = {
                 x:2,
                 y:3,
                 z:true
-            }
+            };
 
             var b = {
                 x:5,
                 z:false
-            }
+            };
 
             var result = {
                 x:5,
                 y:3,
                 z:false
-            }
+            };
 
             var photomosaic = new PhotoMosaic({
                 'image': image
@@ -65,5 +45,5 @@ describe('PhotoMosaic', function() {
                 expect(photomosaic.extend(a,b)).to.eql(result);
             });
         });
-    })
-})
+    });
+});
